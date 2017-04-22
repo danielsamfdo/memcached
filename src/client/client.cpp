297d939837c79d9b25545e44dc7d7982f92f8e1c
Pankaj Bhambhani <pbhambhani@cs.umass.edu>
2017-04-22 02:14:36 -0400

    Converted C code to C++

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,25 +1,27 @@
 //Ref - http://www.geeksforgeeks.org/socket-programming-cc/
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <string.h>
 #include <arpa/inet.h>
 #include <boost/algorithm/string/predicate.hpp>
+#include <common/log_util.h>
 #define PORT 8080
+
+using namespace std;
 
 int main(int arg_count, char const *argv[]) {
 
     int client_socket, read_len, connect_return;
     struct sockaddr_in server_address;
     char buffer[1024] = {0};
-    std::string client_message ("Hello from client! Mujhse fraandship karoge ?\r\n");
+    string client_message ("Hello from client! Mujhse fraandship karoge ?\r\n");
 
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     if(client_socket < 0) {
-        perror("Failed to create client socket");
+        log_error  << "Failed to create client socket" << endl;
         exit(-1);
     }
 
@@ -29,31 +31,31 @@ int main(int arg_count, char const *argv[]) {
     server_address.sin_port = htons(PORT);
 
     if(inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
-        perror("Address is invalid or not supported");
+        log_error  << "Address is invalid or not supported" << endl;
         exit(-1);
     }
 
     connect_return = connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address));
 
     if(connect_return < 0) {
-        perror("Could not connect to server, maybe we're not made for each other");
+        log_error  << "Could not connect to server, maybe we're not made for each other" << endl;
         exit(-1);
     }
 
-    printf("Connected to server, telling him %s\n", client_message.c_str());
+    log_info << "Connected to server, telling him " << client_message.c_str() << endl;
     send(client_socket, client_message.c_str(), client_message.size(), 0);
-    printf("Done, waiting to hear back\n");
+    log_info << "Done, waiting to hear back" << endl;
 
-    std::string message;
-    std::string suffix("\r\n");
+    string message;
+    string suffix("\r\n");
 
     //while(!boost::algorithm::ends_with(message, suffix)) {
         read_len = read(client_socket, buffer, 1024);
         //message.append(buffer);
     //}
 
-    printf("Server said : %s\n", buffer);
-    printf("Time to die\n");
+    log_info << "Server said : " << buffer << endl;
+    log_info << "Time to die" << endl;
 
     return 0;
 }
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,11 +1,16 @@
 //Ref - http://www.geeksforgeeks.org/socket-programming-cc/
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <boost/algorithm/string/predicate.hpp>
+#include <common/log_util.h>
+#include <common/str_util.h>
 #define PORT 8080
+
+using namespace std;
 
 int main(int arg_count, char const *argv[]) {
 
@@ -16,24 +21,17 @@ int main(int arg_count, char const *argv[]) {
     int options = 1;
     int max_read_len = 10;
 
-    std::string server_message("Hello from client! Mujhse fraandship karoge ?\\r\\n");
-
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     if(server_socket == 0) {
-        perror("Failed to create server socket");
+        log_error  << "Failed to create server socket" << endl;
         exit(-1);
     }
 
-    /*if(fcntl(server_socket, F_SETFL, fcntl(server_socket, F_GETFL) | O_NONBLOCK) < 0) {
-        perror("Failed to set server socket to non-blocking");
-        exit(-1);
-    }*/
 
     if(setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                   &options, sizeof(options))) {
-        perror("Failed to set socket options");
-        exit(-1);
+        log_error  << "Failed to set socket options" << endl;
     }
 
     socket_address.sin_family = AF_INET;
@@ -44,46 +42,42 @@ int main(int arg_count, char const *argv[]) {
                            sizeof(socket_address));
 
     if(bind_return < 0) {
-        perror("Failed to bind server");
+        log_error  << "Failed to bind server" << endl;
     }
-    printf("Server got ears on port %d, you got something to say?\n", PORT);
 
     int listen_return = listen(server_socket, 3) ;
 
     if(listen_return < 0) {
-        perror("Failed to listen, I am behra now");
+        log_error  << "Failed to listen, I am behra now" << endl;
     }
 
-
-
-    printf("Server got ears on port %d, you got something to say?\n", PORT);
+    log_info << "Server got ears on port " << PORT << ", you got something to say?" << endl;
 
     while(true) {
         client_socket = accept(server_socket, (struct sockaddr *)&socket_address,
                                (socklen_t*)&addr_len);
 
         if(client_socket < 0) {
-            perror("Failed to hear you, we are not meant for each other, see ya later");
+            log_error  << "Failed to hear you, we are not meant for each other, see ya later" << endl;
             break;
         }
 
-        printf("Hey there, I got someone to talk to!\n");
+        log_info << "Hey there, I got someone to talk to!" << endl;
 
-        std::string message;
-        std::string suffix("\r\n");
+        string message;
+        string suffix("\r\n");
 
-        while(!boost::algorithm::ends_with(message, suffix)) {
+        while(!ends_with(message, suffix)) {
             read_len = read(client_socket, buffer, max_read_len);
             total_read_len += read_len;
             message.append(buffer, read_len);
-            //memset(buffer, '0', sizeof(buffer));
-            printf("Currently received message:%s, read_len:%d, ending ? %d\n", message.c_str(), read_len, boost::algorithm::ends_with(message.c_str(), suffix.c_str()));
+            log_info << "Currently received message: " << message.c_str() << ", read_len: " << read_len << ", ending ? " << ends_with(message, suffix) << endl;
         }
 
 
-        printf("My new friend says : %s, read_len: %d\nI will echo it back to him\n", message.c_str(), read_len);
+        log_info << "My new friend says : " << message.c_str() << ", read_len: " << read_len << "\nI will echo it back to him" << endl;
         send(client_socket, message.c_str(), message.size(), 0);
-        printf("Done\n");
+        log_info << "Done" << endl;
     }
 
     return 0;
