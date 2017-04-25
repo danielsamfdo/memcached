595cf064de9b0a06bdab9a69aa486d23660db741
Pankaj Bhambhani <pankajb64@gmail.com>
2017-04-24 21:54:04 -0400

    added code to tokenize string *sigh*

--- src/common/str_util.h
+++ src/common/str_util.h
@@ -1,6 +1,8 @@
 #ifndef MEMCACHED_CLONE_BRUTE_FORCE_STR_UTIL_H
 #define MEMCACHED_CLONE_BRUTE_FORCE_STR_UTIL_H
 
+#include <regex>
+
 using namespace std;
 
 //Ref - http://stackoverflow.com/a/20446239/1026535
@@ -10,4 +12,17 @@ bool ends_with(const std::string &str, const std::string &suffix)
            str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
 }
 
+vector<string> tokenize(string command) {
+    vector<string> tokens;
+
+    regex re("\\s+");
+    sregex_token_iterator it(command.begin(), command.end(), re, -1);
+    sregex_token_iterator end;
+
+    for(; it != end; ++it) {
+        tokens.push_back(it->str());
+    }
+
+    return tokens;
+}
 #endif //MEMCACHED_CLONE_BRUTE_FORCE_STR_UTIL_H
