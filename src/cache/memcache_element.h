2d062697ca0ebc4e0d54020402e7ebc66b426c1a
Pankaj Bhambhani <pankajb64@gmail.com>
2017-04-24 22:00:52 -0400

    Added fields to memcache element

--- src/cache/memcache_element.h
+++ src/cache/memcache_element.h
@@ -5,6 +5,10 @@
 
 struct MemcacheElement {
     uint8_t *block;
+    uint16_t flags;
+    int exptime;
+    int bytes;
+    u64 cas_unique;
 };
 
 #endif //MEMCACHED_CLONE_BRUTE_FORCE_MEMCACHE_ELEMENT_H
