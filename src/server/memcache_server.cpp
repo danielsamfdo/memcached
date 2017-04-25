675a3bc4366e80fa7e440ceca8f82e5a7eb1c882
Pankaj Bhambhani <pankajb64@gmail.com>
2017-04-24 20:38:34 -0400

    Added code to init cache, read command and send output

--- src/server/memcache_server.cpp
+++ src/server/memcache_server.cpp
@@ -1,11 +1,59 @@
-MemcacheServer::MemcacheServer(int port) : TCPServer(port) {
-
+MemcacheServer::MemcacheServer(int cache_type, int port) : TCPServer(port) {
+    init(cache_type);
 }
 
-MemcacheServer::MemcacheServer(int port, int max_conn_backlog) : TCPServer(port, max_conn_backlog) {
+MemcacheServer::MemcacheServer(int cache_type, int port, int max_conn_backlog, int max_read_len) : TCPServer(port, max_conn_backlog) {
+    init(cache_type);
+}
 
+void MemcacheServer::init(int cache_type) {
+    max_read_len = 10;
+    suffix = "\r\n";
+
+    switch(cache_type) {
+        case 1:
+            cache = new LRUCache();
+            break;
+        case 2:
+            cache = new RandomReplacementCache();
+            break;
+        case 3:
+            cache = new LandlordCache();
+    }
 }
 
 void MemcacheServer::process_conn(int socket) {
 
+    while(true) {
+        string command = read_command(socket);
+        string output = process_command(socket);
+
+        if(! output.empty()) {
+            send_output(socket, output);
+        }
+    }
+}
+
+string read_command(int socket) {
+
+    string command;
+    char buffer[max_read_len] = {0};
+
+    while(!ends_with(command, suffix)) {
+        int read_len = read(socket, buffer, max_read_len);
+        command.append(buffer, read_len);
+    }
+
+    return command;
+}
+
+string process_command(int socket) {
+
+    return cache.process_command(socket);
+}
+
+void send_output(int socket, string output) {
+
+    output.append(suffix);
+    send(socket, output.c_str(), output.size(), 0);
 }
--- src/server/memcache_server.h
+++ src/server/memcache_server.h
@@ -2,14 +2,24 @@
 #define MEMCACHED_CLONE_BRUTE_FORCE_MEMCACHE_SERVER_H
 
 #include "tcp_server.h"
+#include <cache/memcache.h>
 
 class MemcacheServer : public TCPServer {
 
-public:
-    MemcacheServer(int port);
-    MemcacheServer(int port, int max_conn_backlog);
+private:
+    int max_read_len;
+    string suffix;
+    Memcache cache;
 
+public:
+    MemcacheServer(int cache_type, int port);
+    MemcacheServer(int cache_type, int port, int max_conn_backlog);
+
+    void init();
     void process_conn(int socket);
+    string read_command(int socket);
+    string process_command(int socket);
+    void send_output(int socket, string output);
 };
 
 #include "memcache_server.cpp"
