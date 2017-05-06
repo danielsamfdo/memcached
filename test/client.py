58ed545aac521bec0f4a73dfefe59313717ce23d
Pankaj Bhambhani <pankajb64@gmail.com>
2017-05-06 18:08:51 -0400

    Python test client

--- test/client.py
+++ test/client.py
@@ -2,7 +2,7 @@ import memcache
 
 mc = memcache.Client(['127.0.0.1:8080'], debug=0)
 
-mc.set("some_key", "some bad");
-value = mc.get("some_key");
+mc.set("some_key", "some bad")
+value = mc.get("some_key")
 
 print value
