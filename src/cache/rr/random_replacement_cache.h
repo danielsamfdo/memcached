c28a4cfb94e623300585209853d3d9db6a6324ce
Rohithpesala <rpesala@umass.edu>
2017-05-07 02:30:14 -0400

    Random replacement implemented

--- src/cache/rr/random_replacement_cache.h
+++ src/cache/rr/random_replacement_cache.h
@@ -5,6 +5,37 @@
 
 class RandomReplacementCache : public Memcache {
 
+public:
+
+	RandomReplacementCache();
+
+private:
+
+	int Evict(size_t mem_need)
+	{
+		/*
+		Parameters:
+		:: mem_need :: Amount of memory needed to evict
+		Return:
+		:: returns 1 if evicted the needed memory else returns 0
+		*/
+		size_t claimed = 0;
+		while(claimed<mem_need)
+		{
+			if (cache.size()>0)
+			{
+				claimed += (cache.begin()->second).bytes;
+				cache.erase(cache.begin());
+			}
+			else
+			{
+				return 0;
+			}
+		}
+		//assign the size var to (size-claimed)
+		return 1
+	}
+
 };
 
 #include "random_replacement_cache.cpp"
