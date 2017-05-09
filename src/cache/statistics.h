b0de513053147d9ff51c6ed4f32728f9f7b9abb9
Rohithpesala <rpesala@umass.edu>
2017-05-08 22:48:40 -0400

    minor

--- src/cache/statistics.h
+++ src/cache/statistics.h
@@ -9,7 +9,7 @@ public:
     unsigned long long int start_time;
     unsigned long long int cmd_get, cmd_set, cmd_flush, get_hits, get_misses, delete_misses, delete_hits, incr_misses, incr_hits, decr_misses, decr_hits, cas_misses, cas_hits, reclaimed, bytes_read;
     unsigned long long int limit_maxbytes, evicted_unfetched, expired_unfetched, curr_items, total_items, bytes, version;
-    string process_get_statistics();
+    string process_get_statistics(uint64_t ut, string version);
     Statistics()
     {
     	allocated = 0;
